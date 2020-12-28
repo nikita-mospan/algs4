a47bc965429a42411c2ec0d3a7ecbfc0318edfe4
Nikita Mospan <mospan.nn@phystech.edu>
2020-12-28 13:20:03 +0300

    wordnet: final version

--- src/main/java/WordNet.java
+++ src/main/java/WordNet.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,70 +15,6 @@ public class WordNet {
     private final Map<Integer, Set<String>> synsetIdToNouns;
     private final Set<String> nouns;
     private final SAP sap;
-    private final Map<DistanceForNouns, DistanceForNouns> distanceForNounsMap;
-    private final Map<SapVertexForNouns, SapVertexForNouns> sapVertexForNounsMap;
-
-    private static final class DistanceForNouns {
-        private final String nounA;
-        private final String nounB;
-        private final int distance;
-
-        public DistanceForNouns(String nounA, String nounB) {
-            this.nounA = nounA;
-            this.nounB = nounB;
-            this.distance = -1;
-        }
-
-        public DistanceForNouns(String nounA, String nounB, int distance) {
-            this.nounA = nounA;
-            this.nounB = nounB;
-            this.distance = distance;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            DistanceForNouns that = (DistanceForNouns) o;
-            return nounA.equals(that.nounA) && nounB.equals(that.nounB);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(nounA, nounB);
-        }
-    }
-
-    private static final class SapVertexForNouns {
-        private final String nounA;
-        private final String nounB;
-        private final int sapVertex;
-
-        public SapVertexForNouns(String nounA, String nounB) {
-            this.nounA = nounA;
-            this.nounB = nounB;
-            this.sapVertex = -1;
-        }
-
-        public SapVertexForNouns(String nounA, String nounB, int sapVertex) {
-            this.nounA = nounA;
-            this.nounB = nounB;
-            this.sapVertex = sapVertex;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            SapVertexForNouns that = (SapVertexForNouns) o;
-            return nounA.equals(that.nounA) && nounB.equals(that.nounB);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(nounA, nounB);
-        }
-    }
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
@@ -120,8 +55,6 @@ public class WordNet {
             throw new IllegalArgumentException("digraph does not have single root");
         }
         sap = new SAP(digraph);
-        distanceForNounsMap = new HashMap<>();
-        sapVertexForNounsMap = new HashMap<>();
     }
 
     // returns all WordNet nouns
@@ -151,10 +84,7 @@ public class WordNet {
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
         checkNounsNotNull(nounA, nounB);
-        DistanceForNouns distanceForNouns = distanceForNounsMap.computeIfAbsent(new DistanceForNouns(nounA, nounB),
-                key -> new DistanceForNouns(nounA, nounB, sap.length(getSynsetsWithNoun(nounA), getSynsetsWithNoun(nounB))));
-        distanceForNounsMap.put(new DistanceForNouns(nounB, nounA), distanceForNouns);
-        int result = distanceForNouns.distance;
+        int result = sap.length(getSynsetsWithNoun(nounA), getSynsetsWithNoun(nounB));
         if (result != -1) {
             return result;
         } else {
@@ -166,10 +96,7 @@ public class WordNet {
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
         checkNounsNotNull(nounA, nounB);
-        SapVertexForNouns sapVertexForNouns = sapVertexForNounsMap.computeIfAbsent(new SapVertexForNouns(nounA, nounB),
-                key -> new SapVertexForNouns(nounA, nounB, sap.ancestor(getSynsetsWithNoun(nounA), getSynsetsWithNoun(nounB))));
-        sapVertexForNounsMap.put(new SapVertexForNouns(nounB, nounA), sapVertexForNouns);
-        final int sapVertex = sapVertexForNouns.sapVertex;
+        final int sapVertex = sap.ancestor(getSynsetsWithNoun(nounA), getSynsetsWithNoun(nounB));
         if (sapVertex != -1) {
             return String.join(" ", synsetIdToNouns.get(sapVertex));
         } else {
